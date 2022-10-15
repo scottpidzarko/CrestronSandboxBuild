f629318753af609ee4211d2b866ad815f551a795
Scott Pidzarko <scott.pidzarko@gmail.com>
2022-10-15 11:56:31 -0700

    Update SignForSandboxTask to be drive letter agnostic

--- src/SignForCrestronSandboxTask/SignForSandboxTask.cs
+++ src/SignForCrestronSandboxTask/SignForSandboxTask.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Reflection;
 using Microsoft.Build.Framework;
 using System.Text;
+using static System.Environment.SpecialFolder;
 
 namespace Microsoft.Build.Tasks
 {
@@ -118,29 +119,26 @@ namespace Microsoft.Build.Tasks
         {
             try
             {
-                // TODO delete this section of code - was added for indicating if build was 64-bit or not
-                if (IntPtr.Size == 4)
+                // Constants setup
+                // TODO Expose this to MSBuild 
+                string logfolder = @"SIMPLSharpLogs";
+                string datapath = Path.Combine(Environment.GetFolderPath(ProgramFilesX86), @"Crestron\Cresdb\Programming");
+                bool refreshSdk = true;
+
+                string path;
+                // MSBuild docs say this has no trailing slash, but it appears it really does.
+                // We don't want the trailing slash for the plugin
+                if (projectDir[projectDir.Length - 1].Equals(Path.DirectorySeparatorChar))
                 {
-                    //throw new Exception("32 bit");
-                }
-                else if (IntPtr.Size == 8)
-                {
-                    // 64-bit
-                    //throw new Exception("64 bit");
+                    path = projectDir.Remove(projectDir.Length - 1, 1);
                 }
                 else
                 {
-                    // The future is now!
+                    path = projectDir;
                 }
 
-                // ************ This starts the part for signing the dlls with the crestron plugin ****************
-                // Constants setup
-                string logfolder = @"\SIMPLSharpLogs";
-                string datapath = @"C:\Program Files (x86)\Crestron\Cresdb\Programming";
-                bool refreshSDK = true;
 
-                string path = projectDir.Remove(projectDir.Length - 1, 1); // docs say this has no trailing slash, but it appears it really does. We don't want the trailing slash for the plugin
-                string logpath = path + logfolder;
+                string logpath = Path.Combine(path, logfolder);
 
                 string absoluteOutputPath = targetDir;
                 string fullAssemblyName = targetPath;
@@ -148,14 +146,30 @@ namespace Microsoft.Build.Tasks
                 string archiveFullName = Path.Combine(absoluteOutputPath, compiledFileName);
 
                 // setup the SIMPLSharp service
-                var bf = BindingFlags.NonPublic | BindingFlags.Instance;
+                const BindingFlags bf = BindingFlags.NonPublic | BindingFlags.Instance;
                 var p = new SIMPLSharpPackage();
                 Type t = p.GetType();
                 var method = t.GetMethod("d", bf);
-                method.Invoke(p, null);
+                if (method != null)
+                {
+                    method.Invoke(p, null);
+                }
+                else
+                {
+                    throw new Exception("Could not initialize the SimplSharp package. Initialization method was null.");
+                }
+
                 var data = t.GetProperty("ProviderConfig", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(p, null);
 
-                var s = new SIMPLSharpService(data, targetName, path, logpath, refreshSDK, datapath);
+                SIMPLSharpService s;
+                if (data != null)
+                {
+                    s = new SIMPLSharpService(data, targetName, path, logpath, refreshSdk, datapath);
+                }
+                else
+                {
+                    throw new Exception("Could not initialize SmplSharpService from plugin. Data was null.");
+                }
 
                 // these three fields are set up outside of making the simpl sharp service for some reason
                 if (s.ProjectInfo.CompiledFilename == null)
@@ -175,15 +189,17 @@ namespace Microsoft.Build.Tasks
                     s.SetMinimumFirmwareVersion("1.007.0017"); // TODO figure out how to systematically generate this?
                 }
 
-                //SDK resources not set by the SIMPLSharpProject class or any decendants
-                //set by SIMPLSharpPackage			
+                // SDK resources not set by the SIMPLSharpProject class or any decendants
+                // set by SIMPLSharpPackage			
                 // the vsplugin builds this by comparing the project references with the list of sdkassemblies from SimplSharp service 
                 // and the system assemblies from simplsharp service, and the deprecated assemblies from simplsharp service
-                IList<ReferenceInfo> projectReferences = new List<ReferenceInfo>(); // TODO add code to generate this. For the televic example it doesn't have extra references so this is blank
+                IList<ReferenceInfo> projectReferences = new List<ReferenceInfo>(); // TODO add code to generate this. Can we get from MSBuild?
 
-                // I can't remember what the purpose of the result of this was in the actual plugin. TODO determine what to do with the result here - tests didn't use the result
+                // I can't remember what the purpose of the result of this was in the actual plugin.
+                // TODO determine what to do with the result here - tests didn't use the result
                 s.ReconcileProjectReferences(projectReferences);
-                // I can't remember what the purpose of the result of this was in the actual plugin. TODO determine what to do with the result here - tests didn't use the result
+                // I can't remember what the purpose of the result of this was in the actual plugin.
+                // TODO determine what to do with the result here - tests didn't use the result
                 s.IsAssemblyVerified(fullAssemblyName);
 
                 IList<string> resolutionPaths = new List<string>
@@ -191,9 +207,9 @@ namespace Microsoft.Build.Tasks
                     absoluteOutputPath,
                     SIMPLSharpService.SDKPath
                 };
-                // string passed is the full assembly name, which we get from Simplsharp project FullAssemblyName
+                // string passed is the full assembly name, which we get from SimplSharp project FullAssemblyName
                 // for generateDebugSysmbols, we *may* have to set this to false in order to get Mono.Cecil to like the pdb format
-                // TODO determine if we need to true/false and if that can depend on the project debug/release configuratino
+                // TODO determine if we need to true/false and if that can depend on the project debug/release configuration passed in from MSBuild
                 var result1 = s.ValidateAssembly(fullAssemblyName, resolutionPaths, false);
                 if (!result1.IsValid)
                 {
@@ -201,7 +217,7 @@ namespace Microsoft.Build.Tasks
 
                     foreach (var error in result1.Errors)
                     {
-                        sb.Append(String.Format("Sandbox validation error: {0}\r\n", error));
+                        sb.Append($"Sandbox validation error: {error}\r\n");
                     }
                     throw new Exception(sb.ToString());
                 }
@@ -228,7 +244,7 @@ namespace Microsoft.Build.Tasks
                         ReferenceInfo item = new ReferenceInfo
                         {
                             CopyLocal = true,
-                            Filename = text, // full assembly name for file - dll or exe, ie. @"C:\Users\Username\OtherDirs\ProjecDir\bin\Debug\Newtonsoft.Json.Compact.dll"
+                            Filename = text, // full assembly name for file - dll or exe, ie. @"C:\Users\Username\OtherDirs\ProjectDir\bin\Debug\Newtonsoft.Json.Compact.dll"
                             ReferenceType = ReferenceType.Assembly,
                             IsVerified = true
                         };
@@ -238,20 +254,23 @@ namespace Microsoft.Build.Tasks
 
                 // list of everything in the output path that doesn't have .info, .pdb, .config, .clz, .cplz, .cpz, .dll, and .exe? Unsure if it actually does that, it builds a list of that and never returns it.
                 // list of everything in the SimplSharpProject's SDK resources enumerator then gets copied to the output path and also added to this list
-                var references = new List<string>();
+                var simplSharpReferences = new List<string>();
                 foreach (ResourceFileInfo resourceFileInfo in SIMPLSharpService.SDKResources)
                 {
-                    if (!references.Contains(resourceFileInfo.Filename, StringComparer.OrdinalIgnoreCase))
+                    if (!simplSharpReferences.Contains(resourceFileInfo.Filename, StringComparer.OrdinalIgnoreCase))
                     {
                         string text = Path.Combine(absoluteOutputPath, resourceFileInfo.Filename);
                         File.Copy(resourceFileInfo.Fullname, text, true);
-                        references.Add(text);
+                        simplSharpReferences.Add(text);
                     }
                 }
 
                 Log.LogMessage("Creating project manifest");
-                s.CreateManifest(fullAssemblyName, compiledFileName, dependencies, references);
+                s.CreateManifest(fullAssemblyName, compiledFileName, dependencies, simplSharpReferences);
 
+                // If the project depends on the Crestron SDK nuget package to do the archiving we don't need to use the
+                // below. Included for posterity
+                /*
                 IList<string> ListOfDllsInProjectInfo;
                 // I think the below is how the plugin does it.
                 ListOfDllsInProjectInfo = SIMPLSharpService.SystemAssemblies
@@ -259,10 +278,11 @@ namespace Microsoft.Build.Tasks
                     .Distinct()
                     .ToList<string>();
 
-                /* s.CreateArchive(
+                 s.CreateArchive(
 					absoluteOutputPath,
 					ListOfDllsInProjectInfo,
-					archiveFullName); */                
+					archiveFullName); 
+				*/                
 
                 return true;
 
@@ -270,6 +290,7 @@ namespace Microsoft.Build.Tasks
             catch (Exception ex)
             {
                 Log.LogErrorFromException(ex);
+                Log.LogError(ex.StackTrace);
                 return false;
             }
         }
